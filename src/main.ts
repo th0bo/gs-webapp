0ac2fa2b747528eeb35dc905332d11b6f8304cdb
Théo Boulaire <th.boulaire@gmail.com>
2024-03-02 09:00:30 +0100

    Enhanced basic web app

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
 const doGet = (e: GoogleAppsScript.Events.DoGet) => {
-  return ContentService.createTextOutput(`test GET ${JSON.stringify(e)}`);
-}
+  console.log("get");
+  const htmlOutput = HtmlService.createTemplateFromFile("src/index").evaluate();
+  return htmlOutput;
+};
 
 const doPost = (e: GoogleAppsScript.Events.DoPost) => {
-  return ContentService.createTextOutput(`test POST ${JSON.stringify(e)}`);
-}
+  console.log("post");
+  const params = JSON.stringify(e);
+  return ContentService.createTextOutput(params).setMimeType(
+    ContentService.MimeType.JSON
+  );
+};
+
+const include = (filename) =>
+  HtmlService.createHtmlOutputFromFile(filename).getContent();
+
+const getOrderedList = () =>
+  Tasks.Tasks?.list("@default")
+    .items?.map((task) => task.title)
+    .map((text) => `<li>${text}</li>`)
+    .join("");
